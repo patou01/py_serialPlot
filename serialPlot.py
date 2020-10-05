3d6b7de036548bc0a49ee1836a47272c3d532cac
Patrick Bobbink <pbobbink@gmail.com>
2020-10-05 19:16:39 +0200

    first working setup with settings file, added an input simulator

--- serialPlot.py
+++ serialPlot.py
@@ -1,164 +1,113 @@
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
-from io import BytesIO
 import serial
 import time
 import platform
+import sys
+import json
+import jsonschema
 
+settingsFile = 'settings.json'
+schema = 'schema.json'
 
-#### SETTINGS 
+def main():
+	print("Using python: " + platform.python_version())
+	print("Using numpi: " + np.__version__)
+	print("Using matplotlib: " + matplotlib.__version__)
+	print("Serial: " + serial.__version__)
 
-# serial port
-serialPort = '/COM4'
-baud = 115200
-
-# number of points to view
-windowSize = 2000
-
-# number of data to plot (not including x)
-nData = 6;
-
-
-# if separatePlots is True, each set is plotted on a different subplot
-# all subplots have same X however.
-separatePlots = False
-
-# allow custom labels for data
-useCustomLabels = True
-# if using custom labels, you are responsible for having as many entries as nData
-customLabels = ['sinx', 'sin2x'] 
-
-
-# if true, use first input from serial as x axis, else just plot it
-firstInputAsX = True
-# Only used to estimate the x interval to display at the beginning
-# dt is ~ the time expected between 2 points. 
-# Only used when firstInputAsX is True
-dt = 0.0100 
-
-
-
-# y auto resize. If autoResizeY set to False, please give minY < maxY
-autoResizeY = False
-minY = -1
-maxY = 1
-
-
-
-
-######  END SETTINGS
-
-
-print("Using python: " + platform.python_version())
-print("Using numpi: " + np.__version__)
-print("Using matplotlib: " + matplotlib.__version__)
-print("Serial: " + serial.__version__)
-
-ser = serial.Serial(serialPort, baud)
-
-x = np.array([0.0])
-y = np.zeros(shape=(1,nData))
-lines = []
-ax = []
-
-intX = int(firstInputAsX)
-
-
-
-if firstInputAsX:
-	dataNames = 'x'
-else:
-	dataNames = 'y0'
+	with open(settingsFile, 'r') as f, open(schema, 'r') as ref:
+		settings = json.load(f)
+		refSettings = json.load(ref)
+		jsonschema.validate(settings, refSettings)
 		
-		
-dNames = [dataNames]
-for i in range(int(not firstInputAsX), nData):
-	dataNames = dataNames + ', y' + str(i) 
-	dNames = np.append(dNames, 'y' + str(i))
+		serialPort = settings['port']
+		baud = settings['baud']
+		nData = settings['nInputs']
+		firstInputAsX = settings['firstInputAsX']
+		dt = settings['dt']
+		windowSize = settings['windowSize']
+		autoResizeY = settings['autoSizeY']
+		minY = settings['yMin']
+		maxY = settings['yMax']
+		useCustomLabels = settings['useCustomLabels']
+		customLabels = settings['dataLabels']
+		displayTime = 1.0/settings['displayRate']
+
+	x = np.array([0.0])
+	y = np.zeros(shape=(1,nData))
+	lines = []
+	ax = []
+
+	# make figure
+	plt.ion()
+	fig = plt.figure()
+
+	ax = fig.add_subplot(111)
+	ax.set_autoscale_on(True)
+	ax.autoscale_view(True,True,True)
+
+	# add lines to plot
+	for i in range(0, nData):
+		lines = np.append(lines, ax.plot(x, y[:,i]))
 
 
-
-# dataTypes for parsing serial line
-dataTypes = []
-for i in range(0, intX+nData): # 1+ because of x input
-	dataTypes = np.append(dataTypes, np.dtype(np.float))
+	labels = np.array(customLabels)
+	ax.legend(labels, bbox_to_anchor=(0,1.1), loc="upper left", ncol=nData)
 
 
-# make figure
-plt.ion()
-fig = plt.figure()
-
-ax = fig.add_subplot(111)
-ax.set_autoscale_on(True)
-ax.autoscale_view(True,True,True)
-
-
-# add lines to plot
-for i in range(0, nData):
-	lines = np.append(lines, ax.plot(x, y[:,i]))
-
-
-# add labels
-if useCustomLabels:
-	ax.legend(customLabels, bbox_to_anchor=(0,1.1), loc="upper left", ncol=nData)
-else:
-	ax.legend(dNames[intX:len(dNames)],bbox_to_anchor=(0,1.1), loc="upper left", ncol=nData)
-
-
-
-lastDisplay = time.time()
-while True:
-	start = time.time()
 	nPoints = 0
-	# read line on serial
-	while ser.inWaiting():
-		
-		
-		# get inputs from serial
-		line = ser.readline()
-		datasplit = str(line, 'ASCII').split(',')
-		inputs = [float(f) for f in datasplit]
-		nPoints = nPoints + 1
-								
-		# add data to arrays
-		if x.size < windowSize:
-			x = np.append(x, inputs[0] if firstInputAsX == True else x.size)
-			y = np.vstack([y, np.array(inputs[int(firstInputAsX):])])
+	with serial.Serial(serialPort, baud) as ser:
+		lastDisplay = time.time()
+		while True:
 			
-		else:
-			x = np.roll(x, -1)
-			x[-1] = inputs[0] if firstInputAsX == True else nPoints
-			
-			y = np.roll(y, -1, axis=0)
-			y[-1] = np.array(inputs[int(firstInputAsX):])
-			
-			
-			
-		# update line data for plots		
-		i = 0
-		for l in lines:
-				l.set_ydata(y[:,i])
-				l.set_xdata(x)
-				i = i+1
+			while ser.inWaiting():
+				# get inputs from serial
+				line = ser.readline()
+				datasplit = str(line, 'utf-8').split(',')
+				inputs = [float(f) for f in datasplit]
+				nPoints = nPoints + 1
+				
+				# add data to arrays
+				if x.size < windowSize:
+					x = np.append(x, inputs[0] if firstInputAsX == True else x.size)
+					y = np.vstack([y, np.array(inputs[int(firstInputAsX):])])
+				else:
+					x = np.roll(x, -1)
+					x[-1] = inputs[0] if firstInputAsX == True else nPoints
+					y = np.roll(y, -1, axis=0)
+					y[-1] = np.array(inputs[int(firstInputAsX):])
+					
+				# update line data for plots		
+				i = 0
+				for l in lines:
+						l.set_ydata(y[:,i])
+						l.set_xdata(x)
+						i = i+1
 
-	end = time.time()
-	lastDisplay = time.time()
-	
-	ax.autoscale_view(True,x.size < windowSize,autoResizeY)
-	if not autoResizeY:
-		ax.set_ylim(minY, maxY)
-		
-	if x.size < windowSize :
-		if firstInputAsX:
-			ax.set_xlim(0, dt*windowSize)
-		else:
-			ax.set_xlim(0, windowSize)		
-	else:
-		ax.set_xlim(x[x.size-windowSize], x[x.size-1], True, True)
-	
-	
-	ax.relim()
-	fig.canvas.draw()
-	fig.canvas.flush_events()
-		
+
+			# update display
+			t = time.time()
+			if(t - lastDisplay > displayTime):
+				lastDisplay = t
+				ax.autoscale_view(True,x.size < windowSize,autoResizeY)
+				if not autoResizeY:
+					ax.set_ylim(minY, maxY)
+					
+				if x.size < windowSize :
+					if firstInputAsX:
+						ax.set_xlim(0, dt*windowSize)
+					else:
+						ax.set_xlim(0, windowSize)		
+				else:
+					ax.set_xlim(x[0], x[-1], True, True)
+				
+				ax.relim()
+				fig.canvas.draw()
+				fig.canvas.flush_events()
+			
+			time.sleep(0.5*dt)
+
+if __name__ == '__main__':
+	main()
--- serial_input_faker.py
+++ serial_input_faker.py
@@ -3,50 +3,52 @@ import sys
 import msvcrt
 from time import sleep
 import math
+import sys
+import json
+import jsonschema 
 
-# serial port
-serialPort = '/COM3'
-baud = 921600
-
-
-# number of data to plot (not including x)
-nData = 6;
-
-
-# if true, use first input from serial as x axis, else just plot it
-firstInputAsX = True
-
-dt = 0.0100 
-
-def initSerial(port):
-	print("starting up serial port {port}")
-
+settingsFile = 'settings.json'
+schema = 'schema.json'
 
 def main():
 	run = True
+
+	with open(settingsFile, 'r') as f, open(schema, 'r') as ref:
+		settings = json.load(f)
+		refSettings = json.load(ref)
+		jsonschema.validate(settings, refSettings)
 		
-	initSerial(serialPort)
+		serialPort = settings['testPort']
+		baud = settings['baud']
+		nData = settings['nInputs']
+		firstInputAsX = settings['firstInputAsX']
+		dt = settings['dt']
 	
 	with serial.Serial(serialPort, baud) as ser:
-		#ser.write('hello'.encode('utf-8'))
 		i = 0
-		while run:
-			points = []
-			for j in range(0,nData):
-				points.append(math.sin(j*i*dt))
+		points = []
+		for j in range(0,nData):
+			points.append(0)
 			
+		while run:
+			for j in range(0,nData):
+				points[j] = round(math.sin((j+1)*i*dt),2)
 			points_str = str(points).strip('[]')
 			
-			outstr = f"{i*dt:.2f} , {points_str} \n"
+			if firstInputAsX :
+				outstr = f"{i*dt:.2f} , {points_str} \n"
+			else:
+				outstr = f"{points_str} \n"
+
 			ser.write(outstr.encode('utf-8'))
-	#		print(outstr)
+
 			i = i+1
-			kbhit = msvcrt.kbhit()
-			if kbhit == True:
+			if msvcrt.kbhit() == True:
 				c = msvcrt.getch().decode('ASCII')
 				ex = 'q'
-				print(f"received {c}, exit is {ex}")
+				
 				if c == 'q':
+					print(f"received {c}, exit is {ex}")
 					run = False
 		
 			sleep(dt)
